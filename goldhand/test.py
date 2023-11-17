a07ca386bf828fd5cd14d1d8c92fdbb2a2149229
Mihály Orsós <ormraat.pte@gmail.com>
2023-11-17 18:40:36 +0100

    9

--- goldhand/test.py
+++ goldhand/test.py
@@ -8,10 +8,3 @@ tw =Tw()
 # p = t.plotly_last_year(tw.get_plotly_title(stock_ticker))
 # p.update_layout(height=1080, width=1920)
 # p.write_image("fig2.png", )
-
-tickers_df= tw.get_top_n_stocks_by_sector()
-
-print(tickers_df.head())
-
-create_video(list(tickers_df['name'][1:20]), '/home/mihaly/Videos/try.mp4')
-
