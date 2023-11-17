7aa1ce62c0a08568bee03ea0a1a9c2f501e2b769
Mihály Orsós <ormraat.pte@gmail.com>
2023-11-17 18:06:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="goldhand",
-    version="3.21",
+    version="4.0",
     author="Mihaly",
     author_email="ormraat.pte@gmail.com",
     description="A package working with financial data",
     url="https://github.com/misrori/goldhand",
     license="MIT",
     install_requires=['pandas_datareader', 'pandas', 'pandas_ta', 'plotly', 'scipy', 
-                      'numpy', 'requests', 'cloudscraper', 'temp_file', 'tqdm'],
+                      'numpy', 'requests', 'cloudscraper', 'tqdm'],
     packages=find_packages(),
     # other arguments omitted
     long_description=long_description,
