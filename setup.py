7f856676e73712359e8e16e02a2316551c47102d
Mihály Orsós <ormraat.pte@gmail.com>
2025-09-26 10:32:16 +0200

    Update package version and add numba dependency

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="goldhand",
-    version="17.3",
+    version="17.4",
     author="Mihaly",
     author_email="ormraat.pte@gmail.com",
     description="A package working with financial data",
     url="https://github.com/misrori/goldhand",
     license="MIT",
-    install_requires=['pandas_datareader', 'pandas==2.2.2', 'pandas_ta', 'plotly', 'scipy', 'numpy==1.26.4',
+    install_requires=['pandas_datareader', 'pandas==2.2.2', 'pandas_ta==0.4.71b0', 'plotly', 'scipy', 'numpy==1.26.4', 'numba==0.61.2',
                       'numpy', 'requests', 'cloudscraper', 'tqdm'],
     packages=find_packages(),
     # other arguments omitted
