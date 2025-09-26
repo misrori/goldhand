5e0692dfb29d713f4253dbe478493dc0ee5dfad0
Mihály Orsós <ormraat.pte@gmail.com>
2025-09-26 10:23:48 +0200

    Bump version to 17.3 and update pandas requirement

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="goldhand",
-    version="17.2",
+    version="17.3",
     author="Mihaly",
     author_email="ormraat.pte@gmail.com",
     description="A package working with financial data",
     url="https://github.com/misrori/goldhand",
     license="MIT",
-    install_requires=['pandas_datareader', 'pandas', 'pandas_ta', 'plotly', 'scipy', 'numpy==1.26.4',
+    install_requires=['pandas_datareader', 'pandas==2.2.2', 'pandas_ta', 'plotly', 'scipy', 'numpy==1.26.4',
                       'numpy', 'requests', 'cloudscraper', 'tqdm'],
     packages=find_packages(),
     # other arguments omitted
