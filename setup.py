8f2ff080ac98c337d9f244f22ac54098efbcbd5b
Mihály Orsós <ormraat.pte@gmail.com>
2025-09-26 10:40:41 +0200

    Update version to 17.5 and dependencies in setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="goldhand",
-    version="17.4",
+    version="17.5",
     author="Mihaly",
     author_email="ormraat.pte@gmail.com",
     description="A package working with financial data",
     url="https://github.com/misrori/goldhand",
     license="MIT",
-    install_requires=['pandas_datareader', 'pandas==2.2.2', 'pandas_ta==0.4.71b0', 'plotly', 'scipy', 'numpy==1.26.4', 'numba==0.61.2',
+    install_requires=['pandas_datareader', 'pandas', 'pandas_ta', 'plotly', 'scipy', 'numpy', 'numba',
                       'numpy', 'requests', 'cloudscraper', 'tqdm'],
     packages=find_packages(),
     # other arguments omitted
